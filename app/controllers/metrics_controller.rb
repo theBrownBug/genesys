8e66e8e9046f5addb8b155448ee98b34f222cccf
Ryan Walton <rwalton2@sheffield.ac.uk>
2021-11-14 15:15:34 +0000

    added metrics_controller

---
+++ app/controllers/metrics_controller.rb
@@ -0,0 +1,15 @@
+ class MetricsController < ApplicationController
+    def create
+        from = Time.at(params["pageVisitedFrom"].to_i / 1000).to_datetime
+        to = Time.at(params["pageVisitedTo"].to_i / 1000).to_datetime
+    
+        Visit.create(from: from,
+          to: to,
+          path: params['path'],
+          csrf_token: params['authenticity_token'],
+          session_identifier: session.id
+        )
+    
+        head :ok
+      end
+    
