e1a9d70b21cab7de5bf7fda4f36f3cd7519480e1
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2021-11-17 13:25:02 +0000

    add user tracking in devise

--- db/migrate/20211117114437_add_devise_to_users.rb
+++ db/migrate/20211117114437_add_devise_to_users.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      change_column :users, :email, :string, null: false , default: ""
+      change_column :users, :email, :string, null: false , default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -15,11 +15,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -46,6 +46,13 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+
+    change_table :users do |t|
+      t.remove :encrypted_password
+      t.remove :reset_password_token
+      t.remove :reset_password_sent_at
+      t.remove :remember_created_at
+    end
+
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,19 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
 #  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
 #  password               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
 #  user_type              :integer
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
