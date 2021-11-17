85808d7df4b0d5e6e3d9b5e5e5663b758c541161
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2021-11-17 13:45:16 +0000

    make fields in users table mandatory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,8 +7,8 @@
 #  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  first_name             :string
-#  last_name              :string
+#  first_name             :string           not null
+#  last_name              :string           not null
 #  last_sign_in_at        :datetime
 #  last_sign_in_ip        :string
 #  password               :string
