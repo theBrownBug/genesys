56bfdff768362a5ef847594d2750e89c8968d43e
Laney Deveson <ldeveson1@sheffield.ac.uk>
2021-11-18 19:07:41 +0000

    wffef

--- app/models/register.rb
+++ app/models/register.rb
@@ -4,11 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  email      :string
-<<<<<<< HEAD
 #  option     :integer
-=======
-#  poption    :integer
->>>>>>> 8a7fbcf0f8dc4b4988c3bfaf22d5043044cb5a6e
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
