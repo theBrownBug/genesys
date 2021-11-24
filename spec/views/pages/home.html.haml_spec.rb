282fc061845263c12b308cd3269824863477ed16
Jen Ollett <jmollett1@sheffield.ac.uk>
2021-11-24 19:48:26 +0000

    Add some tests

--- spec/views/pages/home.html.haml_spec.rb
+++ spec/views/pages/home.html.haml_spec.rb
@@ -34,4 +34,26 @@ describe 'pages/home' do
       expect(rendered).to have_selector('p', text: 'based on the credit weights of your modules, allowing you to focus on the actual studying.')
     end
   end
+
+  describe '#landing_features' do
+    it 'displays the title' do
+      render
+      expect(rendered).to have_selector('h2', text: 'A timetable to suit you')
+    end
+
+    it 'displays the subtitle' do
+      render
+      expect(rendered).to have_selector('p', text: 'Feature spotlight')
+    end
+
+    # rubocop:disable Rspec/MultipleExpectations
+    it 'displays the feature titles' do
+      render
+      expect(rendered).to have_selector('h6', text: 'Automated timetable creation')
+      expect(rendered).to have_selector('h6', text: 'Account for last minute changes')
+      expect(rendered).to have_selector('h6', text: 'Track repeating tasks')
+      expect(rendered).to have_selector('h6', text: 'Interested?')
+    end
+    # rubocop:enable Rspec/MultipleExpectations
+  end
 end
