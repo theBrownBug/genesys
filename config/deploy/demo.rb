ad06d8ad136a69a5480d5ac0d40c3e3264045b56
Laney Deveson <ldeveson1@sheffield.ac.uk>
2021-11-24 14:36:40 +0000

    deployment changes

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
 ## Application deployment configuration
-set :server,      'DEMO_SERVER'
-set :user,        'DEMO_USER'
+
+set :server,      'epi-stu-gen-demo1.shef.ac.uk'
+
+set :user,        'demo.team05'
 set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
 set :log_level,   :debug
+set :rails_env,   :demo
 
 ## Server configuration
-server fetch(:server), user: fetch(:user), roles: %w[web app db]
+server fetch(:server), user: fetch(:user), roles: %w{web app db}
 
 ## Additional tasks
 namespace :deploy do
   task :seed do
-    on primary :db do
-      within current_path do
-        with rails_env: fetch(:stage) do
-          execute :rake, 'db:seed'
-        end
-      end
-    end
+    on primary :db do within current_path do with rails_env: fetch(:stage) do
+      execute :rake, 'db:seed'
+    end end end
   end
 end
