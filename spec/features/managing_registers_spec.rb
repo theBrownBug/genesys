cc8bf1407b03ddc0b3de5c34749200a054c29a7b
Laney Deveson <ldeveson1@sheffield.ac.uk>
2021-11-28 20:58:09 +0000

    Cosmetic changes

--- spec/features/managing_registers_spec.rb
+++ spec/features/managing_registers_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
-    describe 'Adding register' do
-        specify 'I can add a product' do
+describe 'Adding register' do
+    specify 'I can add a register' do
         visit '/'
-        click_link 'Register Interest'
+        click_link "I'm interested!"
         fill_in 'Email', with: 'anExample@example'
         choose('Option 1')
         click_button 'Register Interest'
         expect(page).to have_content 'anExample@example'
-        #within(:css, 'table') { expect(page).to have_content 'Kenwood - 500 watt' }
+        #expect(page).to have_content 'Option You Choose: 2'
+    end
 
+    specify 'I can add a register - option 2' do
+        visit '/'
+        click_link "I'm interested!"
+        fill_in 'Email', with: 'anExample@example'
+        choose('Option 2')
+        click_button 'Register Interest'
+        expect(page).to have_content 'Option You Choose: 2'
     end
 end
+
