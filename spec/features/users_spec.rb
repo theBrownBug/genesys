fdee4feae5f0c856f13e101b3fc470b07680570e
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2021-12-09 18:24:30 +0000

    add user and role data in factory bot

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+let(:admin)         { build(:role , role_type:1)}
+let(:product_owner) { build(:role , role_type:2)}
+let(:reporter)      { build(:role , role_type:3)}
+
+before
+
+
+describe 'Create users' do
+
+end
