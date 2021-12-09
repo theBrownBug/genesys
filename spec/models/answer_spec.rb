d6ba5aeb217e3164d94b3d1d41b1071f5fa8bb26
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2021-12-09 10:55:10 +0000

    update tests in Answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -23,6 +23,13 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+
+  before do
+    @question = FactoryBot.build(:question)
+    @answer = FactoryBot.build(:answer)
+    @answer.question = @question
+  end
+
   it { should belong_to(:question) }
   subject {described_class.new}
 
@@ -70,5 +77,21 @@ RSpec.describe Answer, type: :model do
     expect(subject).not_to be_valid
   end
 
-
 end
+
+
+# In order to be a valid Answer, it should have a question_id
+describe Answer do
+  before do
+    @question = FactoryBot.build(:question)
+    @answer = FactoryBot.build(:answer)
+    @answer.question = @question
+  end
+  subject {@answer}
+  it "is a valid anwer" do
+    expect(subject).to be_valid
+  end
+end
+
+
+
